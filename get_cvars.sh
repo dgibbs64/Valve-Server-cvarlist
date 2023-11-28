db5cbe6298d9372bcd9e5b683c0fbba6debdff89
Daniel Gibbs <me@danielgibbs.co.uk>
2023-11-28 21:59:24 +0000

    add actions

---
+++ get_cvars.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+shortname="css"
+mkdir linuxgsm
+cd linuxgsm || exit
+wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh ${shortname}
+./cssserver auto-install
+./cssserver start
+sleep 10
+./cssserver send cvarlist
+cp log/console/cssserver-console.log ../cvarlist.txt
+./cssserver stop
+
+# remove all lines before "cvar list"
+sed -i '1,/cvar list/d' ../cvarlist.txt
+
+# remove all lines after "convars/concommands"
+sed -i '/convars\/concommands/,$!d' ../"${shortname}-cvarlist.txt"
+
+rm -rf linuxgsm
