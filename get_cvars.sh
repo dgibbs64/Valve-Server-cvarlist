fc665e224e9caef3e3ed69762c22c7320f038a70
Daniel Gibbs <me@danielgibbs.co.uk>
2023-11-28 22:11:38 +0000

    fixes

--- get_cvars.sh
+++ get_cvars.sh
@@ -7,13 +7,13 @@ wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm
 ./cssserver start
 sleep 10
 ./cssserver send cvarlist
-cp log/console/cssserver-console.log ../cvarlist.txt
+cp log/console/cssserver-console.log ../"${shortname}-cvarlist.txt"
 ./cssserver stop
 
 # remove all lines before "cvar list"
-sed -i '1,/cvar list/d' ../cvarlist.txt
+sed -i '1,/cvar list/d' ../"${shortname}-cvarlist.txt"
 
 # remove all lines after "convars/concommands"
 sed -i '/convars\/concommands/,$!d' ../"${shortname}-cvarlist.txt"
-
+cd ../ || exit
 rm -rf linuxgsm
