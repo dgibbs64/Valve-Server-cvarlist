6c9ccd857a661d465783ae3172405d13084d3033
Daniel Gibbs <me@danielgibbs.co.uk>
2023-11-29 12:51:52 +0000

    test

---
+++ .github/workflows/detals-check-generate-matrix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo -n "{" > "shortnamearray.json"
+echo -n "\"include\":[" >> "shortnamearray.json"
+
+while read -r line; do
+	shortname=$(echo "$line" | awk -F, '{ print $1 }')
+	export shortname
+	echo -n "{" >> "shortnamearray.json"
+	echo -n "\"shortname\":" >> "shortnamearray.json"
+	echo -n "\"${shortname}\"" >> "shortnamearray.json"
+	echo -n "}," >> "shortnamearray.json"
+done < <(tail -n serverlist.csv)
+sed -i '$ s/.$//' "shortnamearray.json"
+echo -n "]" >> "shortnamearray.json"
+echo -n "}" >> "shortnamearray.json"
