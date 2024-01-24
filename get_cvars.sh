dd0e1fdbccddf7c010348ad4e0822afa13317ce3
Daniel Gibbs <me@danielgibbs.co.uk>
2024-01-24 12:46:59 +0000

    fixes

--- get_cvars.sh
+++ get_cvars.sh
@@ -3,16 +3,29 @@ shortname="${1}"
 mkdir linuxgsm
 cd linuxgsm || exit
 wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh ${shortname}
+
 ./${shortname}server auto-install
 ./${shortname}server start
 sleep 10
 ./${shortname}server send cvarlist
-cp log/console/${shortname}server-console.log ../"${shortname}-cvarlist.txt"
+sleep 10
+
+echo "Display console log"
+if [[ ! -s "log/console/${shortname}server-console.log" ]]; then
+    echo "The console log is empty"
+else
+    cat "log/console/${shortname}server-console.log"
+fi
+
+cp "log/console/${shortname}server-console.log" ../"${shortname}-cvarlist.txt"
 ./${shortname}server stop
 
 # remove all lines before "cvar list"
 echo "Removing all lines before \"cvar list\""
 sed -ni -Ee '/cvar list/,$ p' ../"${shortname}-cvarlist.txt"
+
+echo "Display cvarlist"
+cat ../"${shortname}-cvarlist.txt"
 
 echo "Tidy"
 cd ../ || exit
